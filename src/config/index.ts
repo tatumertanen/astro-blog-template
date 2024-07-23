276c54537d5c0836faa4aa3ce10b491c5f3e1f5d
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-23 20:16:47 +0300

    Create Nav config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,25 @@
-import type { Site } from '@/types'
+import type { Site, Nav } from '@/types'
 
 export const site: Site = {
     name: "Astro Boilerplate",
     description: "Astro Boilerplate with basic integrations including TailwindCSS",
 }
+
+export const nav: Nav = [
+    {
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "About",
+        path: "/about"
+    },
+    {
+        name: "Blog",
+        path: "/blog"
+    },
+    {
+        name: "Contact",
+        path: "/contact"
+    }
+]
