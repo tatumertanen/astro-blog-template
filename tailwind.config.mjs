f1d9d4f1b38d4dffca39a2c64217a5ffe8785deb
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-23 20:16:01 +0300

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+				serif: ['Merriweather', 'serif'],
+			},
 			spacing: {
 				'8xl': '96rem',
 				'9xl': '128rem',
